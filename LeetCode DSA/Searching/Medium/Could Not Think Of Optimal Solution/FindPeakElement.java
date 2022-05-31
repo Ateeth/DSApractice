2e2d92bba42ad481a833e9daa43a690bbd15a9fd
Ateeth Arun <58625369+Ateeth@users.noreply.github.com>
2022-05-31 11:19:58 +0400

    Update FindPeakElement.java

--- LeetCode DSA/Searching/Medium/Could Not Think Of Optimal Solution/FindPeakElement.java
+++ LeetCode DSA/Searching/Medium/Could Not Think Of Optimal Solution/FindPeakElement.java
@@ -4,6 +4,7 @@ package com.ateeth.LeetCodeDSA.searching.medium.CouldNotThinkOfOptimalSolution;
 import java.util.Scanner;
 
 public class FindPeakElement {
+   /*
     public static int findPeakElement(int[] nums) {
         int N = nums.length;
         if (N == 1) {
@@ -20,10 +21,36 @@ public class FindPeakElement {
             }
         }
 
+    
+    //    if left is last element return that index
+      //  if left is greater than its next element return left
+
+    //    else return right
+    
+    //    return (left == N - 1 || nums[left] > nums[left + 1]) ? left : right;
+    }
+
+    */
+    
+     public static int findPeakElement(int[] nums) {
+        int N = nums.length;
+        if (N == 1) {
+            return 0;
+        }
+
+        int left = 0, right = N - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < nums[mid + 1]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
     /*
         if left is last element return that index
         if left is greater than its next element return left
-
         else return right
     */
         return (left == N - 1 || nums[left] > nums[left + 1]) ? left : right;
